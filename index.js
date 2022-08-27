776c7347640948ecf9cd0afd76b1848829f7071f
+ <+>
2022-08-27 21:57:24 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    //compare firstletter with last letter
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+
+  return true;
 }
+
 
 /* 
   Add your pseudocode here
+ -If the **first letter** is the same as the **last
+ -letter**, and the **second letter** is the same as the **second to last**
+ -letter, and so on (until the middle of the word), the function should return
+`true`.
+
 */
 
 /*
